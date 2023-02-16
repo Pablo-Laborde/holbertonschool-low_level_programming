37d0e25c4d2c632e275ee1baf969fc39bd7de977
Pablo-Laborde <p5.tacorontee@gmail.com>
2023-02-16 14:52:04 -0800

    5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main- Prints the phrase the task that was given to us.
+ *
+ * Return: 0 always.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}

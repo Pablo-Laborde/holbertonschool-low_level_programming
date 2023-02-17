ea16116bd229923dcc804ef91738a30768ae9622
Pablo-Laborde <p5.tacorontee@gmail.com>
2023-02-17 08:13:14 -0800

    9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main- prints all possible combinations of single-digit
+ * numbers
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	/* var declaration */
+	int i;
+
+	/* code */
+	i = 0;
+	do {
+		putchar(i + '0');
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		else
+			putchar('\n');
+		i++;
+	} while (i < 10);
+}

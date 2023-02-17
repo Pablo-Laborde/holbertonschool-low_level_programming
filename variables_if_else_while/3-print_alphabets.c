bccfffcebedeccd06d6443a40d70210ab29e1d78
Pablo-Laborde <p5.tacorontee@gmail.com>
2023-02-17 06:05:35 -0800

    3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main- prints the alphabet in lowercase and uppercase
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	/* var declaration */
+	char c;
+
+	/* functions */
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (c + 'A'; c <= 'Z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}

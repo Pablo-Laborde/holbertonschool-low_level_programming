62dc090cc9c010e64cea9af51d00da1916c6d83a
Pablo-Laborde <p5.tacorontee@gmail.com>
2023-02-17 07:59:59 -0800

    8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main- prints all the numbers of base 16 in lowercase
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	/* var declaration */
+	int i;
+	char c;
+
+	/* code */
+	for (i = 0; i < 10; i++)
+		putchar((char)(i));
+	for (c = 'a'; c  <= 'f'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}

cb77f75c4d390118328881f067b9efaed211db9a
Pablo-Laborde <p5.tacorontee@gmail.com>
2023-02-17 05:13:11 -0800

    re1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -12,16 +12,22 @@
 int main(void)
 {
 	int n;
+	int m;
+	char *s1;
+	char *s2;
+	char *s3;
+	char *s4;
+	char *s5;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	int m = n % 10;
-	char *s1 = "Last digit of ";
-	char *s2 = " is ";
-	char *s3 = " and is grater than 5\n";
-	char *s4 = " and is 0\n";
-	char *s5 = " and is less than 6 and not 0\n";
+	m = n % 10;
+	s1 = "Last digit of ";
+	s2 = " is ";
+	s3 = " and is grater than 5\n";
+	s4 = " and is 0\n";
+	s5 = " and is less than 6 and not 0\n";
 
 	if (m > 5)
 	{

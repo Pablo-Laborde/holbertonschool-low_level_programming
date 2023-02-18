a0f67ab88f694b788f7b0bdb8297accade35575c
Pablo-Laborde <p5.tacorontee@gmail.com>
2023-02-18 15:16:15 -0800

    1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/**
+ * print_alphabet- prints the alphabet fallowed by a new line
+ *
+ * Return: none as its void
+ */
+void print_alphabet(void)
+{
+	/* var decalarion */
+	char c;
+
+	/* code*/
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+}

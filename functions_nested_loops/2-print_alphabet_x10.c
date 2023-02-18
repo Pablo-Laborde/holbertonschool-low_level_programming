615e064b839a81d2d6df1b0c01af10dda341cc4a
Pablo-Laborde <p5.tacorontee@gmail.com>
2023-02-18 15:24:40 -0800

    2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10- print 10 times the alphabet fallowed by a new line
+ *
+ * Return: none asi its void
+ */
+void print_alphabet_x10(void)
+{
+	/* var declaration */
+	int i;
+	char c;
+
+	/* code */
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 'a'; c = 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}

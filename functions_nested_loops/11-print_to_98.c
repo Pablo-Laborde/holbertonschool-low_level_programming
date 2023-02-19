f4ff1013943a60c4eb027adb3805058093da53ea
Pablo-Laborde <p5.tacorontee@gmail.com>
2023-02-18 22:58:01 -0800

    re11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,22 +9,44 @@
  */
 void print_to_98(int n)
 {
-	while (n != 98)
-	{
-		if (n > 10)
-			_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-		_putchar(',');
-		_putchar(' ');
+	/* code */
+	do {
+		print_number(n);
 		if (n < 98)
 			n++;
 		else
 			n--;
-	}
-	if (n == 98)
-	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
+	} while (n != 98);
 	_putchar('\n');
+}
+
+/**
+ * print_number- prints number
+ *
+ * @n: passed by others
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	/* var declaration */
+	int length, aux;
+
+	/* code */
+	aux = n;
+	length = 0;
+	while (aux != 0)
+	{
+		length++;
+		aux /= 10;
+	}
+	if (n < 0)
+		_putchar('-');
+	while (length > 1)
+	{
+		_putchar((n / (10 ** length)) + '0');
+		n %= (10 ** length);
+		length--;
+	}
+	_putchar((n % 10) + '0');
 }

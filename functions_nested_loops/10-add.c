7c33014a7b33503cb90ad40fcff8ce00d44b5691
Pablo-Laborde <p5.tacorontee@gmail.com>
2023-02-18 21:50:34 -0800

    10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add- adds two integers
+ *
+ * @n: passed by main
+ * @m: passed by main
+ *
+ * Return: the addition of n and m
+ */
+int add(int n, int m)
+{
+	return (n + m);
+}

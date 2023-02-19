879f65ba4b11bcd8fac065e664c2d4916fbc87e6
Pablo-Laborde <p5.tacorontee@gmail.com>
2023-02-18 20:42:39 -0800

    9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,1 +1,34 @@
 #include "main.h"
+
+/**
+ * times_table- 9 times table
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	/* var declaration */
+	int i, j, k, x, y;
+
+	/* code */
+	i = 0;
+	j = 0;
+	while (i < 10 && j < 10)
+	{
+		k = i * j;
+		if (j > 0 && j < 9)
+		{
+			_putchar(',');
+			y = (k < 10) ? 2 : 1;
+			for (x = 0; x < y; x++)
+				_putchar(' ');
+		}
+		if (k < 10)
+			_putchar(k + '0');
+		else
+		{
+			_putchar((k / 10) + '0');
+			_putchar((k % 10) + '0');
+		}
+	}
+}

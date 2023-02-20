300b26e8cd0bc1b95d533868561b87d18ba8a5d1
Pablo-Laborde <p5.tacorontee@gmail.com>
2023-02-20 08:11:32 -0800

    100

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+
+
+void print_number(int n);
+void print_comaspace(int n);
+
+/**
+ * print_times_table- prints n times table
+ *
+ * @n: passed
+ *
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	/* var declaration */
+	int i, j, k;
+
+	/* code */
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i < n; i++)
+			for (j = 0; j < n; j++)
+			{
+				k = i * j;
+				if (j == 0)
+					print_number(k);
+				else if (j > 0 && j < n)
+				{
+					print_comaspace(k);
+					print_number(k);
+				}
+				else
+					print_number(k);
+			}
+		putchar('\n');
+	}
+}
+
+/**
+ * print_number- prints number
+ *
+ * @n: passed
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	if (n < 10)
+		putchar(n + '0');
+	else if (n < 100)
+	{
+		putchar((n / 10) + '0');
+		putchar((n % 10) + '0');
+	}
+	else
+	{
+		putchar((n / 100) + '0');
+		putchar(((n % 100) / 10) + '0');
+		putchar((n % 10) + '0');
+	}
+}
+
+/**
+ * print_comaspace- prints coma and space
+ *
+ * @n: passed
+ *
+ * Return: void
+ */
+void print_comaspace(int n)
+{
+	/* var declaration */
+	int i, k;
+
+	/* code */
+	putchar(',');
+	if (n < 10)
+		k = 3;
+	else if (n < 100)
+		k = 2;
+	else
+		k = 1;
+	for (i = 0; i < k; i++)
+		putchar(' ');
+}

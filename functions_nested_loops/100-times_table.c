21ca16e8f9b571689acd15d18920479912fda4c6
Pablo-Laborde <p5.tacorontee@gmail.com>
2023-02-20 08:17:17 -0800

    re100

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -20,6 +20,7 @@ void print_times_table(int n)
 	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i < n; i++)
+		{
 			for (j = 0; j < n; j++)
 			{
 				k = i * j;
@@ -33,7 +34,8 @@ void print_times_table(int n)
 				else
 					print_num(k);
 			}
-		putchar('\n');
+			putchar('\n');
+		}
 	}
 }
 

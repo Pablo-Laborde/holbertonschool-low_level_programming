23a14ac9fe5c3254ee0f008bfc3644697b594593
Pablo-Laborde <p5.tacorontee@gmail.com>
2023-02-22 05:02:07 -0800

    3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_number- prints number
+ *
+ * Return: void
+ */
+void print_number(void)
+{
+	/* var declaration */
+	char c;
+
+	/* code */
+	for (c = '0'; c <= '9'; c++)
+		_putchar(c);
+}

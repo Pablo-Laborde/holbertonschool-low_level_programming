c1817694e33246e3499b26c6969ad4b59535ac55
Pablo-Laborde <p5.tacorontee@gmail.com>
2023-02-22 06:41:38 -0800

    9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * print_fizzbuzz- prints
+ *
+ * Return:
+ */
+void print_fizzbuzz(void)
+{
+	/* var declaration */
+	int i;
+
+	/* code */
+	for (i = 1; i < 101; i++)
+	{
+		if (i > 1 && i < 100)
+			printf(" ");
+		if (i % 3 == 0)
+			printf("Fizz");
+		if (i % 5 == 0)
+			printf("Buzz");
+		printf("%d", i);
+	}
+	printf("\n");
+}

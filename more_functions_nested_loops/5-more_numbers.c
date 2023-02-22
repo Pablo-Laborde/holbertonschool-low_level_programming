e346f493c0d3b4f241677a59b8fcd4d8bbda050f
Pablo-Laborde <p5.tacorontee@gmail.com>
2023-02-22 05:26:10 -0800

    5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers(void)
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	/* var declaration */
+	int i, x;
+
+	/* code */
+	for (i = 0; i < 10; i++)
+		for (x = 0; x < 15; x++)
+			if (x > 10)
+				_putchar((x / 10) + '0');
+			else
+				_putchar((x % 10) + '0');
+	_putchar('\n');
+}

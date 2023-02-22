98a57a500c56d15bc1009dcb5c6bebf090ed08c8
Pablo-Laborde <p5.tacorontee@gmail.com>
2023-02-22 07:14:39 -0800

    10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -17,7 +17,7 @@ void print_triangle(int size)
 	{
 		for (j = 0; j < (size - 1 - i); j++)
 			_putchar(' ');
-		for (k = (size -1 -i); k < size; k++)
+		for (k = (size - 1 - i); k < size; k++)
 			_putchar('#');
 	}
 }

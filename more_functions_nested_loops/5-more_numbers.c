eb17640c9be067c8a2d7670a2ab1bab018c0f836
Pablo-Laborde <p5.tacorontee@gmail.com>
2023-02-22 05:30:27 -0800

    re5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -12,11 +12,13 @@ void more_numbers(void)
 
 	/* code */
 	for (i = 0; i < 10; i++)
+	{
 		for (x = 0; x < 15; x++)
 		{
 			if (x > 10)
 				_putchar((x / 10) + '0');
 			_putchar((x % 10) + '0');
 		}
-	_putchar('\n');
+		_putchar('\n');
+	}
 }

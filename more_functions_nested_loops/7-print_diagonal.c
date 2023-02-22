caf5d4d19fddda24bc4af23f703c949b9b24e40b
Pablo-Laborde <p5.tacorontee@gmail.com>
2023-02-22 05:53:55 -0800

    7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/** print_diagonal- prints
+ *
+ * @n: input
+ *
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	/* var declaration */
+	int i, j;
+
+	/* code */
+	if (n > 0)
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < (i - 1); j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	else
+		_putchar('\n');
+}

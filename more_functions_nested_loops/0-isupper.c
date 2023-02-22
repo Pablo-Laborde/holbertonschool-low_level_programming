00fe1ebeb12183e8e78ad60345e1b690bd7eb2bf
Pablo-Laborde <p5.tacorontee@gmail.com>
2023-02-22 04:25:27 -0800

    0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper- checks if input is uppercase
+ *
+ * @c: input
+ *
+ * Return: if true 1, else 0
+ */
+int _isupper(int c)
+{
+	/* var declaration */
+	int flag;
+
+	/* code */
+	if (c >= 'A' || c <= 'Z')
+		flag = 1;
+	else
+		flag = 0;
+	return (flag);
+}

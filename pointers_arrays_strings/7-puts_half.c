478ba76b915bf797cce482cd5274a0ec67bb29d6
Pablo-Laborde <p5.tacoronte@gmail.com>
2023-02-27 11:09:52 -0800

    7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,1 +1,22 @@
 #include "main.h"
+
+/**
+ * puts_half- function
+ *
+ * @str: input
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	/* var declaration */
+	int i, j;
+
+	/* code */
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	for (j = (i / 2); j < i; j++)
+		_putchar(str[j]);
+	_putchar('\n');
+}

c89212acd04d0625c3e6eb169e6db1a0d4714147
Pablo-Laborde <p5.tacoronte@gmail.com>
2023-02-27 06:10:03 -0800

    4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_rev- function
+ *
+ * @s: input
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	print_rev_rec(s);
+	_putchar('\n');
+}
+
+/**
+ * print_rev_rec- fucntion
+ *
+ * @s: input
+ *
+ * Return: void
+ */
+void print_rev_rec(char *s)
+{
+	if (s[0] != '\0')
+	{
+		print_rev_rec(s[1]);
+		_putchar(s[0]);
+	}
+}

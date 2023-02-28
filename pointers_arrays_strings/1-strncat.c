284b1f63f84cddebf2239e44d1d28de2dc0852e7
Pablo-Laborde <p5.tacoronte@gmail.com>
2023-02-28 11:38:54 -0800

    1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncat- function
+ *
+ * @dest: input1
+ * @src: input2
+ * @n: input3
+ *
+ * Return: char*
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	/* var declaration */
+	int i, j;
+
+	/* code */
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while ((j < n) && (src[j] != '\n'))
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	return (dest);
+}

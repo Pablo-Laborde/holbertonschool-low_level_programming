cede489e662d0c3125319e36e8278842cb47efce
Pablo-Laborde <p5.tacoronte@gmail.com>
2023-03-01 03:57:01 -0800

    re7

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -11,11 +11,10 @@ char *leet(char *c)
 {
 	/* var declaration */
 	int i, j;
-	char arr[8];
+	char arr[8] = {'o', 'l', '\0', 'e', 'a', '\0', '\0', 't'};
 
 	/* code */
 	i = 0;
-	arr[] = {'o', 'l', '\0', 'e', 'a', '\0', '\0', 't'};
 	while (c[i] != '\0')
 	{
 		for (j = 0; j < 8; j++)

2e91fbb44cbff5a3a73015658cc42b49772f2868
Pablo-Laborde <p5.tacoronte@gmail.com>
2023-03-01 05:48:57 -0800

    re5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -12,23 +12,25 @@
 char *_strstr(char *haystack, char *needle)
 {
 	/* var declaration */
-	int i, j, flag;
+	int i, j, k, flag;
 
 	/* code */
 	i = 0;
 	flag = 0;
-	while ((haystack[i] != '\0') && (flag))
+	while ((haystack[i] != '\0') && (flag == 0))
 	{
 		j = 0;
 		if (needle[j] == '\0')
 			flag = 1;
-		while ((haystack[i] == needle[j]) && (haystack[i] != '\0') && (needle[j] != '\0'))
+		if ((haystack[i] == needle[j]))
 		{
-			i++;
-			j++;
+			k = 0;
+			while ((haystack[i + k] == needle[j + k]) && (haystack[i + k] != '\0')
+					&& (needle[j + k] != '\0'))
+				k++;
+			if (needle[j + k] == '\0')
+				flag = 2;
 		}
-		if (needle[j] == '\0')
-			flag = 2;
 		else
 			i++;
 	}

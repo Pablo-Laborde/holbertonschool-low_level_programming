210a51b0d59971cae3c216daa2c9a8e6ba0dca29
Pablo-Laborde <p5.tacoronte@gmail.com>
2023-03-01 07:29:44 -0800

    7

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagsums- function
+ *
+ * @a: input1
+ * @size: input2
+ *
+ * Return: void
+ */
+void print_diagsums(int *a, int size)
+{
+	/* var declaration */
+	int i, d1, d2;
+
+	/* code */
+	d1 = 0;
+	d2 = 0;
+	for (i = 0; i < size; i++)
+	{
+		d1 += a[i][i];
+		d2 += a[i][size - 1 - i];
+	}
+}

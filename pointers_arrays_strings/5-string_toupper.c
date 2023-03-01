a21fbc531dd1c9008ec19d42621620b44dbe8f25
Pablo-Laborde <p5.tacoronte@gmail.com>
2023-02-28 19:29:41 -0800

    5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper- function
+ *
+ * @c: input
+ *
+ * Return: char*
+ */
+char *string_toupper(char *c)
+{
+	/* var declaration */
+	int i;
+
+	/* code */
+	i = 0;
+	while (c[i] != '\0')
+		if ((c[i] >= 'a') && (c[i] <= 'z'))
+			c[i] -= 'A';
+	return (c);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -19,9 +19,9 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
-char *string_toupper(char *);
-char *cap_string(char *);
-char *leet(char *);
+char *string_toupper(char *c);
+char *cap_string(char *c);
+char *leet(char *c);
 /* project 3 */
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);

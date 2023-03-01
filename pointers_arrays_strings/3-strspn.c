0a4dbe5f4798c14a6aaa719b108a2b0a92c540e1
Pablo-Laborde <p5.tacoronte@gmail.com>
2023-03-01 09:07:04 -0800

    3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -11,4 +11,22 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
+	/* var declaration */
+	int i, j, counter;
 
+	/* code */
+	i = 0;
+	counter = 0;
+	while (s[i] != '\0' || s[i] != ',' || s[i] != ' ' || s[i] != '.' || s[i] != '!' || s[i] != '?' || s[i] != ')')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+				counter++;
+			j++;
+		}
+		i++
+	}
+	return (counter);
+}

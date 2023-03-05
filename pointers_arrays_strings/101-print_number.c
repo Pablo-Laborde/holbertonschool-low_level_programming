3a516ffc61084755cb082966a49ab63f355073d4
Pablo-Laborde <p5.tacoronte@gmail.com>
2023-03-05 06:50:56 -0800

    101

---
+++ pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * print_number- function
+ *
+ * @n: input
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	/* var declaration */
+	int i, j, len, bt;
+
+	/* code */
+	i = n;
+	len = 0;
+	do {
+		i /= 10;
+		len++;
+	} while (i > 0);
+	if (n < 0)
+		_putchar('-');
+	for (j = (len - 1); j >= 0; j--)
+	{
+		bt = base_ten(j);
+		_putchar(n / bt);
+		n %= bt;
+	}
+}
+
+/**
+ * base_ten- function
+ *
+ * @len: input
+ *
+ * Return: int
+ */
+int base_ten(int len)
+{
+	if (len == 0)
+		return (1);
+	else
+		return (10 * base_ten(len - 1));
+}

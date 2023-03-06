2d186c13a7a246ef0ad42722a90960da3412ee74
Pablo-Laborde <p5.tacoronte@gmail.com>
2023-03-06 09:22:06 -0800

    0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main- function
+ *
+ * @argc: input1
+ * @argv: input2
+ *
+ * Return: always 0
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+}

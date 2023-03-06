a3ffad7b1de6045b7852b2ce06538ae9154a9e61
Pablo-Laborde <p5.tacoronte@gmail.com>
2023-03-06 06:17:04 -0800

    5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion- fucntion
+ *
+ * @n: input
+ *
+ * Return: int
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (0);
+	else if (n == 1)
+		return (1);
+	else
+		return (_sqrt_recursion_aux(n, 2);
+}
+
+int _sqrt_recursion_aux(int n, int b)
+{
+	if ((b * b) == n)
+		return (b);
+	else if (b < (n / 2))
+		return (_sqrt_recursion_aux(n, (b + 1)));
+	else
+		return (-1);
+}

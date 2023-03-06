d2f7a9b8480b057e2a75df99a00a3af3d7edeb3d
Pablo-Laborde <p5.tacoronte@gmail.com>
2023-03-06 11:48:19 -0800

    1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main- function
+ *
+ * @argc: input
+ * @argv: input
+ *
+ * Return: always 0
+ */
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}

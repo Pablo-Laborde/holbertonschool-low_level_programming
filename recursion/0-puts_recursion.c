9b8400008700ced4bf01d621b755d0585e4848f3
Pablo-Laborde <p5.tacoronte@gmail.com>
2023-03-06 04:03:12 -0800

    0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts_recursion- function
+ *
+ * @s: input
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(s[0]);
+	if (s[0] != '\0')
+		_puts_recursion(s[1]);
+}

85c96071d44e69259a9b9bdd25b23f100cd3ea25
Pablo-Laborde <p5.tacoronte@gmail.com>
2023-03-06 16:28:00 -0800

    4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -28,15 +28,9 @@ int main(int argc, char **argv)
 		flag = 0;
 		while ((i < argc) && (flag == 0))
 		{
-			if (argv[i][0] != '0')
-			{
-				aux = atoi(argv[i]);
-				if (aux != 0)
-					sum += aux;
-				else
-					flag = 1;
-			}
-			i++;
+			flag = int_check(argv[i]);
+			if (flag == 0)
+				sum += strtoint(argv[1]);
 		}
 		if (flag == 1)
 		{
@@ -49,4 +43,60 @@ int main(int argc, char **argv)
 			return (0);
 		}
 	}
+}
+
+/**
+ * int_check- function
+ *
+ * @c: input
+ *
+ * Return: int
+ */
+int int_check(char *c)
+{
+	/* var declaration */
+	int i, flag;
+
+	/* code */
+	i = 0;
+	flag = 0;
+	while ((c[i] != '\0') && (flag == 0))
+		if ((c[i] < '0') || (c[i] > '9'))
+			flag = 1;
+		else
+			i++;
+	return (flag);
+}
+
+/**
+ * strtoint- function
+ *
+ * @c: input
+ *
+ * Return: int
+ */
+int strtoint(char *c)
+{
+	/* var declaration */
+	int i, sum, sign;
+
+	/* code */
+	i = 0;
+	sum = 0;
+	if (c[0] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else
+		sign = 1;
+	while (c[i] != '\0')
+	{
+		sum *= 10;
+		if (sign == 1)
+			sum += c[i];
+		else
+			sum -= c[i];
+	}
+	return (sum);
 }
--- argc_argv/main.h
+++ argc_argv/main.h
@@ -1,2 +1,4 @@
 int _putchar(char c);
 int main(int argc, char **argv);
+int int_check(char *c);
+int strtoint(char *c);

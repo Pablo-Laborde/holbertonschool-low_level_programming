afd6b964ef4ad388d4c1aa0f0c70524177a4326c
Pablo-Laborde <p5.tacoronte@gmail.com>
2023-03-08 11:31:21 -0800

    5

---
+++ argc_argv/100-change.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main- function
+ *
+ * @argc: input
+ * @argv: input
+ *
+ * Return:
+ */
+int main(int argc, char **argv)
+{
+	/* var declaration */
+	int n, sum;
+
+	/* code */
+	if (argc != 2)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	else
+	{
+		sum = 0;
+		n = atoi(argv[1]);
+		sum += (n / 25);
+		n %= 25;
+		sum += (n / 10);
+		n %= 10;
+		sum += (n / 5);
+		n %= 5;
+		sum += (n / 2);
+		n %= 2;
+		sum += (n / 1);
+		n %= 1;
+		printf("%d\n", n);
+		return (0);
+	}
+}

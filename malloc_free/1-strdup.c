71dc967f897aa588a075b9e11f6059517729ec4a
Pablo-Laborde <p5.tacoronte@gmail.com>
2023-03-13 05:43:55 -0700

    1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup- function
+ *
+ * @str: input
+ *
+ * Return: char
+ */
+char *_strdup(char *str)
+{
+	/* var declaration */
+	int i ;
+	char *string;
+
+	/* code */
+	string = malloc(sizeof(str));
+	if (string != NULL)
+	{
+		i = 0;
+		while (str[i] != '\0')
+			string[i] = str[i];
+		string[i] = '\0';
+	}
+	return (string);
+}

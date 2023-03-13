3ef36ccf5f9708b83b3c5a020c41b31114380c2a
Pablo-Laborde <p5.tacoronte@gmail.com>
2023-03-13 08:53:31 -0700

    2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -17,25 +17,22 @@ char *str_concat(char *s1, char *s2)
 
 	/* code */
 	string = NULL;
-	if ((s1 != NULL) || (s2 != NULL))
+	i = 0;
+	j = 0;
+	if (s1 != NULL)
+		while (s1[i] != '\0')
+			i++;
+	if (s2 != NULL)
+		while (s2[j] != '\0')
+			j++;
+	string = malloc((i + j + 1) * sizeof(char));
+	if (string != NULL)
 	{
-		i = 0;
-		j = 0;
-		if (s1 != NULL)
-			while (s1[i] != '\0')
-				i++;
-		if (s2 != NULL)
-			while (s2[j] != '\0')
-				j++;
-		string = malloc((i + j + 1) * sizeof(char));
-		if (string != NULL)
-		{
-			for (k = 0; k < i; k++)
-				string[k] = s1[k];
-			for (l = 0; l < j; l++)
-				string[k + l] = s2[l];
-			string[k + l] = '\0';
-		}
+		for (k = 0; k < i; k++)
+			string[k] = s1[k];
+		for (l = 0; l < j; l++)
+			string[k + l] = s2[l];
+		string[k + l] = '\0';
 	}
 	return (string);
 }

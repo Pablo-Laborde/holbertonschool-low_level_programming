4a26dec1912baff284f2eb683054a27229dfae54
Pablo-Laborde <p5.tacoronte@gmail.com>
2023-03-13 20:30:07 -0700

    0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked- function
+ *
+ * @b: input
+ *
+ * Return: void
+ */
+void *malloc_checked(unsigned int b)
+{
+	/* var declaration */
+	void *pointer;
+
+	/* code */
+	pointer = malloc(b);
+	if (pointer == NULL)
+		return (98);
+	else
+		return (pointer);
+}
